dfeee1038e327eab47ad7503398c690507c92a77
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-02-08 18:25:33 +0100

    Vytvořila jsem třídu Computers Vygenerovala jsem konstruktor a Geters a Seters.

---
+++ src/cz/engeto/ja/Computers.java
@@ -0,0 +1,39 @@
+package cz.engeto.ja;
+
+import java.math.BigDecimal;
+
+public class Computers {
+    private String description;
+    private int yearOfProduction;
+    BigDecimal price;
+
+    public Computers(String description, int yearOfProduction, BigDecimal price) {
+        this.description = description;
+        this.yearOfProduction = yearOfProduction;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
